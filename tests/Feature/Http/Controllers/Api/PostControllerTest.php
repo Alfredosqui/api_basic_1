fea3612307c5f92995e1e6fb89f6d94355983efa
Alfredo Rivas <alfredosqui.777@gmail.com>
2020-09-10 23:18:52 -0500

    Version 0.1 Api Basic

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\User;
+use App\Post;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => 'El Post de prueba'
+        ]);
+
+        $response->assertJsonStructure(['id','title','created_at','updated_at'])
+            ->assertJson(['title' => 'El Post de prueba'])
+            ->assertStatus(201); // Ok, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'El Post de prueba']);
+
+    }
+
+    public function test_validate_title(){
+
+        $user = factory(User::class)->create();
+        
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+
+        // Estatus HTTP 422
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show(){
+
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('GET', "/api/posts/$post->id"); //id=1
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200); //Ok
+    }
+
+    public function test_404_show(){
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts/1000');
+
+        $response->assertStatus(404); 
+    }
+
+    public function test_update(){
+
+        //$this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('PUT', "/api/posts/$post->id", [
+            'title' => 'nuevo'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+        ->assertJson(['title' => 'nuevo'])
+        ->assertStatus(200);
+
+        $this->assertDatabaseHas('posts', ['title' => 'nuevo']);
+    }
+
+    public function test_delete(){
+
+        //$this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('DELETE', "/api/posts/$post->id");
+
+        $response->assertSee(null)
+        ->assertStatus(204); // Sin contenido
+
+        $this->assertDatabaseMissing('posts', ['id' => $post->id]);
+    }
+
+    public function test_index(){
+        
+        $user = factory(User::class)->create();
+
+        factory(Post::class, 5)->create();
+
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts');
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => ['id', 'title', 'created_at', 'updated_at']
+            ]
+        ])->assertStatus(200); //OK
+    }
+
+    public function test_guest(){
+
+        $this->json('GET', '/api/posts')->assertStatus(401);
+        $this->json('POST', '/api/posts')->assertStatus(401);
+        $this->json('GET', '/api/posts/1000')->assertStatus(401);
+        $this->json('PUT', '/api/posts/1000')->assertStatus(401);
+        $this->json('DELETE', '/api/posts/1000')->assertStatus(401);
+    }
+}
